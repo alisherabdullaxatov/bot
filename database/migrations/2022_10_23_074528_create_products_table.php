6b5a5a8376b8043f6967d631fd63b20f31820e12
alisherabdullaxatov <muratjiyankulov@gmail.com>
2022-10-29 19:06:20 +0500

    bot

---
+++ database/migrations/2022_10_23_074528_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('price');
+            $table->string('image')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('status')->default(0);
+            $table->enum('discount_type', ['percent', 'value'])->default('percent');
+            $table->decimal('discount_value')->default(0);
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
